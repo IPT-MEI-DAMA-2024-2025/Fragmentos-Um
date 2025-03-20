2ac590e83e4bd5baf940a566c2760b79362f6180
jcnpereira <casimiro@ipt.pt>
2025-03-20 20:23:35 +0000

    construção da dinâmica da aplicação na 'activity'

--- app/src/main/java/pt/ipt/dama/fragmentosum/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/fragmentosum/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var f1: MeuFragmento
+    lateinit var f2: MeuFragmento
+    lateinit var f3: MeuFragmento
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // instancicar os fragmento
+        f1 = MeuFragmento.newInstance("fragmento um", "botão um")
+        f2 = MeuFragmento.newInstance("Fragmento Dois", "Botão Dois")
+        f3 = MeuFragmento.newInstance("FRAGMENTO TRÊS", "BOTÃO TRÊS")
+
+        // injetar estes três fragmentos na minha interface
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmento1, f1)
+        fragmentTransaction.add(R.id.fragmento2, f2)
+        fragmentTransaction.add(R.id.fragmento3, f3)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
